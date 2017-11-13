e638c32a9ba82f070e18f838639de86aec13bc08
Andy Zhang <zhangandy915@gmail.com>
2017-11-13 11:52:12 -0500

    Update 1.7

--- app/src/main/java/com/examples/android/justjava/MainActivity.java
+++ app/src/main/java/com/examples/android/justjava/MainActivity.java
@@ -2,9 +2,10 @@ package com.examples.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
-
 
 
 /**
@@ -26,24 +27,20 @@ public class MainActivity extends AppCompatActivity {
 
     int numberOfCoffee = 0;
 
-    boolean WhippedCream = false;
-
     public void submitOrder(View view) {
-        String msg = createOrderSummary();
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whippedCream);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        Log.v ("MainActivity", "Has whipped cream: " + hasWhippedCream);
+        String msg = createOrderSummary(hasWhippedCream);
         displaySummary(msg);
 
     }
-
-    public void hasWhippedCream(View view){
-
-    }
-
 
     public void increment(View view) {
         numberOfCoffee = numberOfCoffee + 1;
         displayQuantity(numberOfCoffee);
 
-    }
+}
 
     public void decrement(View view) {
         numberOfCoffee = numberOfCoffee - 1;
@@ -51,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
     }
     /**
 
-     This method displays the given quantity value on the screen.
+     This method displays the given quantity value on the screen.\
      */
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
@@ -72,8 +69,13 @@ public class MainActivity extends AppCompatActivity {
         return price;
     }
 
-    private String createOrderSummary(){
+    private String createOrderSummary(boolean hasWhippedCream){
+        boolean x = hasWhippedCream;
+        x = false;
         String summaryOrder = "Name: CoffeeAddict\n";
+        if (x = true) {
+            summaryOrder += "+Whipped Cream" + "\n";
+        }
         summaryOrder += "Quantity: " + numberOfCoffee + "\n";
         summaryOrder += "Total: " + calculatePrice(numberOfCoffee, 2) + "\n";
         summaryOrder += "Thank you!";
